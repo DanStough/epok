b102200330bf179aab6f4700d918ed33eb59ebd2
DanStough <Daniel.Stough@gmail.com>
2025-07-06 22:17:58 -0400

    doc: add parse module documentation

---
+++ parse/doc.go
@@ -0,0 +1,4 @@
+// Package parse is a module for parsing Unix timestamps into time.Time objects.
+// It attempts to handle ambiguous previsions with a fuzzy parsing approach
+// based on the rules of epochconverter.com.
+package parse
